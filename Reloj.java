8b830492f3094a76f05b66ee334955d8f7d85a94
Nancy <31119437+NancyVazquez27@users.noreply.github.com>
2017-08-31 22:31:48 -0500

    Se realizaron las correcciones y se documento.

--- Reloj.java
+++ Reloj.java
@@ -1,11 +1,13 @@
 
 /**
- * Write a description of class Reloj here.
+ * Esta clase de realizo con la finalidad de simular un reloj digital 24 horas.
+ * Esta clase puede simular avanzar minuto a minuto.
+ * El constructor de la clase recibe como parametro horasIncial, minutosInicial
+ * dadas por el usuario.
  *
  * @author (Nancy Martinez)
  * @version (23.08,17)
  */
-
 public class Reloj
 {
     // instance variables - replace the example below with your own
@@ -14,7 +16,6 @@ public class Reloj
 
    
       // Constructor for objects of class Reloj
-    
     public Reloj(int horasInicial, int minutosInicial)
     {
         // initialise instance variable
@@ -28,23 +29,45 @@ public class Reloj
         // put your code here
         String cadResultado=" ";
         
-        cadResultado=cadResultado+horas;
-        cadResultado=cadResultado+":";
-        cadResultado+=minutos;
-        
+        if(horas>=0 && horas<10)
+            {
+                cadResultado=cadResultado+"0"+horas;
+                cadResultado=cadResultado+":";
+            }   
+        else
+            {
+                cadResultado=cadResultado+horas;
+                cadResultado=cadResultado+":";
+            }
+        if(minutos>=0 && minutos<10)
+        {
+            cadResultado=cadResultado+"0"+minutos;
+        }
+        else
+            {
+                cadResultado=cadResultado+minutos;
+            }
         return cadResultado;
     }
     
     public void ticTac()
     {
-        if(minutos>=0 && minutos<=59)
-        {
-            minutos++; 
-         if (minutos==60)
-         {
-             horas++;
-             minutos=0;
+      if(minutos<=59)
+      {
+          minutos++;
+        }
+      if(minutos==60)
+      {
+          horas++;
+          if(horas==24 && minutos==60)
+          {
+              horas=0;
+              minutos=0;
             }
-        }   
+           if(horas<23 && minutos==60)
+           { 
+               minutos=0;
+            }
+        }
     }
 }
